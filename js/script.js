348c1e886c1fe750cbaa5844b04ac0761feb35ca
jkhvacmd209 <112714380+jkhvacmd209@users.noreply.github.com>
2022-12-23 16:13:18 +0100

    Add files via upload
    Updates voor kleine devices

--- js/script.js
+++ js/script.js
@@ -9,17 +9,17 @@ const frameCount = 438; // Het totaal aantal frames van de animatie
 const startFrame = 120; // Het punt waarop het laden stopt en de animatie op scroll reageert
 
 const currentFrame = index => (
-  `./img/background/background-${index.toString().padStart(3, '0')}.jpeg`
+	`./img/background/background-${index.toString().padStart(3, '0')}.jpeg`
 )
 
 const images = [];
 
 // Alvast alle plaatjes inladen, zodat niet onnodige requests worden gemaakt
 const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    images[i] = new Image();
-    images[i].src = currentFrame(i);
-  }
+	for (let i = 1; i < frameCount; i++) {
+		images[i] = new Image();
+		images[i].src = currentFrame(i);
+	}
 };
 
 preloadImages()
@@ -30,56 +30,60 @@ canvas.height = window.innerHeight;
 
 // Als het scherm hoger is dan het breed is, moet de animatie niet over het hele scherm
 if (canvas.height > canvas.width * 0.8) {
-  canvas.height = canvas.width * 0.8;
+	canvas.height = canvas.width * 0.8;
 }
 
 
 const updateImage = index => {
-  img = images[index];
+	img = images[index];
 
-  // get the scale
-  var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
-  // get the top left position of the image
-  var x = (canvas.width / 2) - (img.width / 2) * scale;
-  var y = (canvas.height / 2) - (img.height / 2) * scale;
-  context.drawImage(img, x, y, img.width * scale, img.height * scale);
+	// get the scale
+	var scale = Math.max(canvas.width / img.width, canvas.height / img.height);
+	// get the top left position of the image
+	var x = (canvas.width / 2) - (img.width / 2) * scale;
+	var y = (canvas.height / 2) - (img.height / 2) * scale;
+	context.drawImage(img, x, y, img.width * scale, img.height * scale);
 }
 
 window.addEventListener('load', function() {
 
-  if (html.scrollTop < window.innerHeight) {
-    for (let i = 1; i < startFrame; i++) {
-      setTimeout(() => {requestAnimationFrame(() => updateImage(i))}, i*1000/50);
-    }
-  }
+	if (html.scrollTop < window.innerHeight) {
+		for (let i = 1; i < startFrame; i++) {
+			setTimeout(() => {requestAnimationFrame(() => updateImage(i))}, i*1000/50);
+		}
+	}
 
 })
 
 window.addEventListener('scroll', () => {  
-  const scrollTop = html.scrollTop;
-  const maxScrollTop = hero.scrollHeight;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - startFrame - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + startFrame))
+	const scrollTop = html.scrollTop;
+	const maxScrollTop = hero.scrollHeight;
+	const scrollFraction = scrollTop / maxScrollTop;
+	const frameIndex = Math.min(
+		frameCount - startFrame - 1,
+		Math.ceil(scrollFraction * frameCount)
+	);
+	
+	requestAnimationFrame(() => updateImage(frameIndex + startFrame))
 });
 
 
 // Text Fade
 
-const titles = document.querySelectorAll("#hero span, #hero p");
-  
-titles.forEach((title) => {
+if (window.innerWidth > 800) {
 
-  title.style.opacity = Math.min(title.getBoundingClientRect().top / canvas.height * 4, (title.getBoundingClientRect().top * -1 + canvas.height) / canvas.height * 4);
+	const titles = document.querySelectorAll("#hero span, #hero p");
+		
+	titles.forEach((title) => {
 
-  window.addEventListener("scroll", () => {
-    title.style.opacity = Math.min(title.getBoundingClientRect().top / canvas.height * 4, (title.getBoundingClientRect().top * -1 + canvas.height) / canvas.height * 4);
-  })
-});
+		title.style.opacity = Math.min(title.getBoundingClientRect().top / canvas.height * 4, (title.getBoundingClientRect().top * -1 + canvas.height) / canvas.height * 4);
+
+		window.addEventListener("scroll", () => {
+			title.style.opacity = Math.min(title.getBoundingClientRect().top / canvas.height * 4, (title.getBoundingClientRect().top * -1 + canvas.height) / canvas.height * 4);
+		})
+	});
+
+}
 
 
 // Code voor skills section
@@ -87,16 +91,16 @@ titles.forEach((title) => {
 const progressbars = document.querySelectorAll("progress");
 
 progressbars.forEach(progressbar => {
-  progressbar.initialvalue = progressbar.value;
-  progressbar.value = 0;
+	progressbar.initialvalue = progressbar.value;
+	progressbar.value = 0;
 
-  window.addEventListener("scroll", () => {
-    if (progressbar.getBoundingClientRect().top < window.innerHeight*3/4) {
-      progressbar.value = progressbar.initialvalue;
-    } else {
-      progressbar.value = 0;
-    }
-  });
+	window.addEventListener("scroll", () => {
+		if (progressbar.getBoundingClientRect().top < window.innerHeight*3/4) {
+			progressbar.value = progressbar.initialvalue;
+		} else {
+			progressbar.value = 0;
+		}
+	});
 });
 
 
@@ -105,12 +109,12 @@ progressbars.forEach(progressbar => {
 const xdFrame = document.querySelector('#adobe-xd-frame');
 
 window.addEventListener("scroll", () => {
-  if (xdFrame.getBoundingClientRect().top / window.innerHeight < 1 && xdFrame.getBoundingClientRect().top / window.innerHeight > 0) {
-    if (xdFrame.dataset.src) {
-      xdFrame.src = xdFrame.dataset.src;
-      delete xdFrame.dataset.src;
-    }
-  }
+	if (xdFrame.getBoundingClientRect().top / window.innerHeight < 1 && xdFrame.getBoundingClientRect().top / window.innerHeight > 0) {
+		if (xdFrame.dataset.src) {
+			xdFrame.src = xdFrame.dataset.src;
+			delete xdFrame.dataset.src;
+		}
+	}
 })
 
 
@@ -130,117 +134,117 @@ let formValid = true;
 const endPoint = "https://script.google.com/macros/s/AKfycbyti1UHlpOxCAnHiVI-bdos9_ZKQ1-5pCanw72WIe0VpMQJpoZzHEsAOf0yqD3fTDfFtw/exec";
 
 firstValidationButton.addEventListener("click", () => {
-  if (nameInput.value == "") {
-    
-    formValid = false;
-    nameInput.setCustomValidity("Vergeet niet je naam in te vullen.");
-    nameInput.classList.add("invalid");
-    nameInput.nextElementSibling.innerText = "Vergeet niet je naam in te vullen.";
-  
-  } else {
-    
-    form.style.marginLeft = "-100vw";
-    nameInput.tabIndex = -1;
-    firstValidationButton.tabIndex = -1;
-    emailInput.tabIndex = 0;
-    secondValidationButton.tabIndex = 0;
-    nameInput.setCustomValidity("");
-    formValid = true;
-    nameInput.classList.remove("invalid");
-    nameInput.nextElementSibling.innerText = "";
-  
-  }
+	if (nameInput.value == "") {
+		
+		formValid = false;
+		nameInput.setCustomValidity("Vergeet niet je naam in te vullen.");
+		nameInput.classList.add("invalid");
+		nameInput.nextElementSibling.innerText = "Vergeet niet je naam in te vullen.";
+	
+	} else {
+		
+		form.style.marginLeft = "-100vw";
+		nameInput.tabIndex = -1;
+		firstValidationButton.tabIndex = -1;
+		emailInput.tabIndex = 0;
+		secondValidationButton.tabIndex = 0;
+		nameInput.setCustomValidity("");
+		formValid = true;
+		nameInput.classList.remove("invalid");
+		nameInput.nextElementSibling.innerText = "";
+	
+	}
 })
 
 secondValidationButton.addEventListener("click", () => {
-  if (emailInput.value == "") {
+	if (emailInput.value == "") {
 
-    formValid = false;
-    emailInput.setCustomValidity("Vergeet niet je e-mail adres in te vullen.");
-    emailInput.classList.add("invalid");
-    emailInput.nextElementSibling.innerText = "Vergeet niet je e-mail adres in te vullen.";
-  
-  } else if (!emailInput.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-    
-    formValid = false;
-    emailInput.setCustomValidity("Vul een correct e-mail adres in");
-    emailInput.classList.add("invalid");
-    emailInput.nextElementSibling.innerText = "Vul een correct e-mail adres in";
-  
-  } else {
-    
-    form.style.marginLeft = "-200vw";
-    emailInput.tabIndex = -1;
-    secondValidationButton.tabIndex = -1;
-    subjectInput.tabIndex = 0;
-    messageInput.tabIndex = 0;
-    submitButton.tabIndex = 0;
-    emailInput.setCustomValidity("");
-    formValid = true;
-    emailInput.classList.remove("invalid");
-    emailInput.nextElementSibling.innerText = "";
-  
-  }
+		formValid = false;
+		emailInput.setCustomValidity("Vergeet niet je e-mail adres in te vullen.");
+		emailInput.classList.add("invalid");
+		emailInput.nextElementSibling.innerText = "Vergeet niet je e-mail adres in te vullen.";
+	
+	} else if (!emailInput.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+		
+		formValid = false;
+		emailInput.setCustomValidity("Vul een correct e-mail adres in");
+		emailInput.classList.add("invalid");
+		emailInput.nextElementSibling.innerText = "Vul een correct e-mail adres in";
+	
+	} else {
+		
+		form.style.marginLeft = "-200vw";
+		emailInput.tabIndex = -1;
+		secondValidationButton.tabIndex = -1;
+		subjectInput.tabIndex = 0;
+		messageInput.tabIndex = 0;
+		submitButton.tabIndex = 0;
+		emailInput.setCustomValidity("");
+		formValid = true;
+		emailInput.classList.remove("invalid");
+		emailInput.nextElementSibling.innerText = "";
+	
+	}
 })
 
 submitButton.addEventListener("click", () => {
 
-  if (subjectInput.value == "") {
-    formValid = false;
-    subjectInput.setCustomValidity("Vergeet niet een onderwerp in te vullen.");
-    subjectInput.classList.add("invalid");
-    subjectInput.nextElementSibling.innerText = "Vergeet niet een onderwerp in te vullen.";
-  } else {
-    subjectInput.setCustomValidity("");
-    formValid = true;
-    subjectInput.classList.remove("invalid");
-    subjectInput.nextElementSibling.innerText = "";
-  }
+	if (subjectInput.value == "") {
+		formValid = false;
+		subjectInput.setCustomValidity("Vergeet niet een onderwerp in te vullen.");
+		subjectInput.classList.add("invalid");
+		subjectInput.nextElementSibling.innerText = "Vergeet niet een onderwerp in te vullen.";
+	} else {
+		subjectInput.setCustomValidity("");
+		formValid = true;
+		subjectInput.classList.remove("invalid");
+		subjectInput.nextElementSibling.innerText = "";
+	}
 
-  if (messageInput.value == "") {
-    formValid = false;
-    messageInput.setCustomValidity("Vergeet je bericht niet.");
-    messageInput.classList.add("invalid");
-    messageInput.nextElementSibling.innerText = "Vergeet je bericht niet.";
-  } else {
-    messageInput.setCustomValidity("");
-    formValid = true;
-    messageInput.classList.remove("invalid");
-    messageInput.nextElementSibling.innerText = "";
-  }
+	if (messageInput.value == "") {
+		formValid = false;
+		messageInput.setCustomValidity("Vergeet je bericht niet.");
+		messageInput.classList.add("invalid");
+		messageInput.nextElementSibling.innerText = "Vergeet je bericht niet.";
+	} else {
+		messageInput.setCustomValidity("");
+		formValid = true;
+		messageInput.classList.remove("invalid");
+		messageInput.nextElementSibling.innerText = "";
+	}
 
-  if(formValid) {
+	if(formValid) {
 
-    submitButton.style.display = "none";
-    loader.style.display = "inline-block";
+		submitButton.style.display = "none";
+		loader.style.display = "inline-block";
 
-    let formData = new FormData();
+		let formData = new FormData();
 
-    formData.append("name", nameInput.value);
-    formData.append("email", emailInput.value);
-    formData.append("subject", subjectInput.value);
-    formData.append("message", messageInput.value);
+		formData.append("name", nameInput.value);
+		formData.append("email", emailInput.value);
+		formData.append("subject", subjectInput.value);
+		formData.append("message", messageInput.value);
 
-    fetch(endPoint, {
-      method: "POST",
-      body: formData
-    })
-    .then((response) => {
-      if (response.status == 200) {
-        form.reset();
-        form.style.marginLeft = "0";
-        successMessage.style.display = "block";
-        successMessage.innerText = "✅ Je bericht is verzonden!";
-        setTimeout(() => {successMessage.style.display = "none"}, 5000)
-        loader.style.display = "none";
-        submitButton.style.display = "inline-block";
-      } else {
-        successMessage.innerText = "❗️ Er is iets mis gegaan, je kan het opnieuw proberen.";
-        loader.style.display = "none";
-        submitButton.style.display = "inline-block";
-      }
-    });
-  }
+		fetch(endPoint, {
+			method: "POST",
+			body: formData
+		})
+		.then((response) => {
+			if (response.status == 200) {
+				form.reset();
+				form.style.marginLeft = "0";
+				successMessage.style.display = "block";
+				successMessage.innerText = "✅ Je bericht is verzonden!";
+				setTimeout(() => {successMessage.style.display = "none"}, 5000)
+				loader.style.display = "none";
+				submitButton.style.display = "inline-block";
+			} else {
+				successMessage.innerText = "❗️ Er is iets mis gegaan, je kan het opnieuw proberen.";
+				loader.style.display = "none";
+				submitButton.style.display = "inline-block";
+			}
+		});
+	}
 
 
 })
